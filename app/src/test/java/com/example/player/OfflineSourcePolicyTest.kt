27e436d05b23d603cb4743f8c33785086d1e9ada
kanunal99-jpg <kanunal99@gmail.com>
2026-09-17 00:14:02 +0300

    test: align offline source policy expectations

--- app/src/test/java/com/example/player/OfflineSourcePolicyTest.kt
+++ app/src/test/java/com/example/player/OfflineSourcePolicyTest.kt
@@ -6,15 +6,15 @@ import org.junit.Test
 
 class OfflineSourcePolicyTest {
     @Test
-    fun allowsVerifiedLocalAndRemoteSchemes() {
+    fun allowsAuthorizedLocalSchemes() {
         assertTrue(OfflineSourcePolicy.isAuthorized("content://media/external/audio/1"))
         assertTrue(OfflineSourcePolicy.isAuthorized("file:///data/user/0/com.example/files/audio.bin"))
-        assertTrue(OfflineSourcePolicy.isAuthorized("https://example.test/audio.mp3"))
-        assertTrue(OfflineSourcePolicy.isAuthorized("http://example.test/audio.mp3"))
     }
 
     @Test
-    fun rejectsUnsupportedSchemes() {
+    fun rejectsRemoteAndUnsupportedSchemes() {
+        assertFalse(OfflineSourcePolicy.isAuthorized("https://example.test/audio.mp3"))
+        assertFalse(OfflineSourcePolicy.isAuthorized("http://example.test/audio.mp3"))
         assertFalse(OfflineSourcePolicy.isAuthorized("rtmp://example.test/audio"))
         assertFalse(OfflineSourcePolicy.isAuthorized("ftp://example.test/audio.mp3"))
         assertFalse(OfflineSourcePolicy.isAuthorized("javascript:alert(1)"))
